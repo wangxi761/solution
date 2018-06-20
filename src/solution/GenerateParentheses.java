027af2bbccc7aabd5e9c32e771b6c68778f357fe
ccf <ccf@LAPTOP-VRVT5L93>
2018-06-20 20:21:27 +0800

    2

---
+++ src/solution/GenerateParentheses.java
@@ -0,0 +1,34 @@
+package solution;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class GenerateParentheses {
+	
+	@Test
+	public void test() throws Exception {
+		List<String> list = generateParenthesis(2);
+		System.out.println(list);
+	}
+	
+	public List<String> generateParenthesis(int n) {
+		List<String> list=new ArrayList<>();
+		generate(list, "", n, n, n);
+		return list;
+	}
+	public void generate(List<String> list,String str,int left,int rigth,int max) {
+		if(left==0&&rigth==0) {
+			list.add(str);
+			return;
+		}
+		if(left>0)
+			generate(list, str+"(", left-1, rigth, max);
+		if(rigth>0&&left<rigth) {
+			generate(list,str+")",left,rigth-1,max);
+		}
+	}
+}
