ebf2cbf025234719e9a9e6ec095f6c0d2e787add
wx224 <wx224@192.168.1.2>
2018-06-21 19:04:06 +0800

    change the package name

---
+++ src/solution/_11_20/ThreeSum.java
@@ -0,0 +1,40 @@
+package solution._11_20;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class ThreeSum {
+	@Test
+	public void test() throws Exception {
+		List<List<Integer>> threeSum = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+		System.out.println(threeSum);
+	}
+	public List<List<Integer>> threeSum(int[] nums) {
+		Set<List<Integer>> list = new HashSet<>();
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length; i++) {
+			int lo = i+1, hi = nums.length - 1;
+			while (lo < hi) {
+				int sum=nums[i]+nums[lo]+nums[hi];
+				if(sum>0) {
+					hi--;
+				}else if(sum <0) {
+					lo++;
+				}else {
+					List<Integer> li=new ArrayList<>();
+					li.add(nums[i]);li.add(nums[lo]);li.add(nums[hi]);
+					list.add(li);
+					hi--;lo++;
+				}
+			}
+		}
+		return new ArrayList<>(list);
+	}
+}
