d5caa708cf7e85c720d65489e90341e6aaf6eee7
wangxi761 <346461036@qq.com>
2019-01-03 14:04:52 +0800

    1

--- src/contest/NumbersWithSameConsecutiveDifferences_967.java
+++ src/contest/NumbersWithSameConsecutiveDifferences_967.java
@@ -4,51 +4,38 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 import org.junit.Test;
 
 public class NumbersWithSameConsecutiveDifferences_967 {
+
 	public int[] numsSameConsecDiff(int N, int K) {
-		int count = 9 - K;
 		List<Integer> list = new ArrayList<>();
-		for (int i = 0; i <= count; i++) {
-			int j = K + i;
-			add(list, combina(i, j, N));
-			add(list, combina(j, i, N));
+		int begin = N == 1 ? 0 : 1;
+		for (int i = begin; i < 10; i++) {
+			dfs(N, K, i, i, 0, list);
 		}
-		int[] d = new int[list.size()];
-		for (int i = 0; i < list.size(); i++) {
-			d[i] = list.get(i);
-		}
-		return d;
+		return list.stream().mapToInt(i -> i).toArray();
 	}
 
-	public void add(List<Integer> list, int num) {
-		if(num<0)
+	public void dfs(int N, int K, int now, int pre, int index, List<Integer> list) {
+		if (index == N - 1) {
+			list.add(now);
 			return;
-		if(!list.contains(num))
-			list.add(num);
-	}
-
-	public int combina(int a, int b, int N) {
-		int result = 0;
-		if (N == 1)
-			return a;
-		else if (a == 0)
-			return Integer.MIN_VALUE;
-		for (int i = 0; i < N / 2; i++) {
-			result = result * 10 + a;
-			result = result * 10 + b;
 		}
-		if (N % 2 == 1)
-			result = result * 10 + a;
-		return result;
+		int x1 = K + pre;
+		if (x1 < 10)
+			dfs(N, K, x1 + now * 10, x1, index + 1, list);
+		int x2 = pre - K;
+		if (x2 != x1 && x2 >= 0)
+			dfs(N, K, x2 + now * 10, x2, index + 1, list);
 	}
 
 	@Test
 	public void test() throws Exception {
-		int[] numsSameConsecDiff = numsSameConsecDiff(1, 6);
+		int[] numsSameConsecDiff = numsSameConsecDiff(3, 7);
 		System.out.println(Arrays.toString(numsSameConsecDiff));
 	}
 }
