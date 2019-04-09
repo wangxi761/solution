5b8b7ff24fa106d1f9c61af1a76ed1f1bdec72f9
wx224 <wx224@192.168.1.2>
2019-04-10 00:14:41 +0800

    0410

---
+++ src/topic/array/FirstMissingPositive.java
@@ -0,0 +1,31 @@
+package topic.array;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        Set<Integer> sets = new HashSet<>();
+        for (int i : nums) {
+            sets.add(i);
+        }
+        int i = 1;
+        while (true) {
+            if (!sets.contains(i))
+                break;
+            else
+                i++;
+        }
+        return i;
+    }
+
+    @Test
+    public void firstMissingPositive1() {
+        int[] arr = {1, 2, 0};
+        int i = firstMissingPositive(arr);
+        System.out.println(i);
+
+    }
+}
