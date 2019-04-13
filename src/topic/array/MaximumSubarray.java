4b6e89b1e996d0bb6177928876e8a1fc3e88d0b2
wangxi761 <346461036@qq.com>
2019-04-13 15:46:33 +0800

    0413

---
+++ src/topic/array/MaximumSubarray.java
@@ -0,0 +1,33 @@
+package topic.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+        }
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    public int maxSubArray2(int[] nums) {
+        int cur = nums[0], pre = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            cur = Math.max(cur + nums[i], nums[i]);
+            pre = Math.max(cur, pre);
+        }
+        return pre;
+    }
+
+    @Test
+    public void maxSubArrayTest() {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSubArray = maxSubArray2(nums);
+        Assert.assertEquals(6, maxSubArray);
+    }
+}
