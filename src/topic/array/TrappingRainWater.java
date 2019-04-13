91de0b489c654e88fb54504b80a68ad6b4ca13ea
wangxi761 <346461036@qq.com>
2019-04-13 11:42:38 +0800

    0410

--- src/topic/array/TrappingRainWater.java
+++ src/topic/array/TrappingRainWater.java
@@ -2,6 +2,7 @@ package topic.array;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class TrappingRainWater {
@@ -21,10 +22,31 @@ public class TrappingRainWater {
         return max;
     }
 
+    public int trap2(int[] height) {
+        int[][] dp = new int[height.length][2];
+        int i = 1;
+        while (i < height.length) {
+            dp[i][0] = Math.max(height[i - 1], dp[i - 1][0]);
+            i++;
+        }
+        i = height.length - 2;
+        while (i >= 0) {
+            dp[i][1] = Math.max(dp[i + 1][1], height[i + 1]);
+            i--;
+        }
+        i = 0;
+        Arrays.stream(dp).forEach(d -> System.out.println(Arrays.toString(d)));
+        for (int j = 0; j < height.length; j++) {
+            int min = Math.min(dp[j][0], dp[j][1]);
+            i += height[j] >= min ? 0 : min - height[j];
+        }
+        return i;
+    }
+
     @Test
-    public void trap1() {
+    public void trapTest() {
         int[] arrs = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int trap = trap(arrs);
+        int trap = trap2(arrs);
         System.out.println(trap);
     }
 }
