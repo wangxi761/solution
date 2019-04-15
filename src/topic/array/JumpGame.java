3644390689243a2429f9b13f2b2ae91b9ee054ff
wangxi761 <346461036@qq.com>
2019-04-15 23:34:11 +0800

    0415

---
+++ src/topic/array/JumpGame.java
@@ -0,0 +1,23 @@
+package topic.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int cur=0;
+        for (int i = 0; i < nums.length; i++) {
+            if(i>cur||cur>= nums.length-1) break;
+            cur=Math.max(i+nums[i],cur);
+        }
+        return cur>=nums.length-1;
+    }
+
+    @Test
+    public void canJumpTest() {
+        int[] arr={2,3,1,1,4};
+        boolean b = canJump(arr);
+        assertTrue(b);
+    }
+}
