d247a94989643732b4eb9abfce04c287bb0c1463
wangxi761 <346461036@qq.com>
2019-04-18 20:28:40 +0800

    0418

---
+++ src/test/java/topic/array/MinimumPathSumTest.java
@@ -0,0 +1,16 @@
+package topic.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinimumPathSumTest {
+
+    @Test
+    public void minPathSum() {
+        int[][] arr = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        MinimumPathSum minimumPathSum = new MinimumPathSum();
+        int i = minimumPathSum.minPathSum(arr);
+        assertEquals(7, i);
+    }
+}
