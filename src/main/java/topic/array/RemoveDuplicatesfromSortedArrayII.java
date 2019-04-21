614abedb0cf94d27923dbabd4cf037a10a9fd6d1
wangxi761 <346461036@qq.com>
2019-04-21 23:07:11 +0800

    0421

--- src/main/java/topic/array/RemoveDuplicatesfromSortedArrayII.java
+++ src/main/java/topic/array/RemoveDuplicatesfromSortedArrayII.java
@@ -3,27 +3,21 @@ package topic.array;
 import java.util.Arrays;
 
 public class RemoveDuplicatesfromSortedArrayII {
-	public int removeDuplicates1(int[] nums) {
+	public int removeDuplicates(int[] nums) {
 		if (nums.length <= 2) return nums.length;
-		int cur = 1, pre = 0, count = 1;
-		while (cur < nums.length) {
-			if (nums[pre] == nums[cur]) {
-				if (count == 1)
-					count++;
-				if (count == 2) {
-					count = 1;
-					cur++;
-				}
-
-			} else {
-				nums[cur] = nums[pre] + 1;
-				pre = cur;
+		int second = 2, first = 2;
+		while (second < nums.length) {
+			if (nums[second] > nums[first - 2]) {
+				nums[first] = nums[second];
+				first++;
 			}
+			second++;
+
 		}
-		return pre;
+		return first;
 	}
 
-	public int removeDuplicates(int[] nums) {
+	public int removeDuplicates2(int[] nums) {
 		int i = 0;
 		for (int num : nums) {
 			if (i < 2 || num > nums[i - 2])
