b0c1e0a13b56615918611ac78306822d89e55579
wangxi761 <346461036@qq.com>
2019-04-29 01:58:26 +0800

    0429

--- src/main/java/topic/array/SearchinRotatedSortedArrayII.java
+++ src/main/java/topic/array/SearchinRotatedSortedArrayII.java
@@ -2,16 +2,25 @@ package topic.array;
 
 public class SearchinRotatedSortedArrayII {
 	public boolean search(int[] nums, int target) {
-		int lo = 0, hi = nums.length - 1;
+		int lo = 0, hi = nums.length - 1, mid = -1;
 		while (lo <= hi) {
-			int mid = (lo + hi) / 2;
+			mid = (lo + hi) / 2;
 			if (nums[mid] == target)
 				return true;
-			else if (nums[mid] > target) {
-
+			else if (nums[mid] > nums[hi] || nums[mid] > nums[lo]) {
+				if (target < nums[mid] && target >= nums[lo])
+					hi = mid - 1;
+				else
+					lo = mid + 1;
+			} else if (nums[mid] < nums[hi] || nums[mid] < nums[lo]) {
+				if (target > nums[mid] && target <= nums[hi])
+					lo = mid + 1;
+				else
+					hi = mid - 1;
+			} else {
+				hi--;
 			}
 		}
-
-		return true;
+		return false;
 	}
 }
--- src/test/java/topic/array/SearchinRotatedSortedArrayIITest.java
+++ src/test/java/topic/array/SearchinRotatedSortedArrayIITest.java
@@ -8,6 +8,10 @@ public class SearchinRotatedSortedArrayIITest {
 
 	@Test
 	public void search() {
-		System.out.println(1);
+		int[] arrs = {2, 5, 6, 0, 0, 1, 2};
+		int[] arrs1 = {3, 1};
+		SearchinRotatedSortedArrayII searchinRotatedSortedArrayII = new SearchinRotatedSortedArrayII();
+		assertTrue(searchinRotatedSortedArrayII.search(arrs, 0));
+		assertTrue(searchinRotatedSortedArrayII.search(arrs1, 1));
 	}
 }
