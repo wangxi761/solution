006c3d38477436ac82eda2abb5f3b8de1a4efbbf
wangxi761 <346461036@qq.com>
2019-05-03 09:42:19 +0800

    0503

--- src/main/java/topic/array/LargestRectangleinHistogram.java
+++ src/main/java/topic/array/LargestRectangleinHistogram.java
@@ -4,10 +4,13 @@ public class LargestRectangleinHistogram {
 	public int largestRectangleArea(int[] heights) {
 		if (heights.length == 0)
 			return 0;
-		int min = heights[0], max = 0;
+		int  max = 0;
 		for (int i = 0; i < heights.length; i++) {
-			min = Math.min(heights[i], min);
-			max = Math.max(max, min * (i+1));
+			int min = heights[i];
+			for (int j = i; j < heights.length; j++) {
+				min = Math.min(heights[j], min);
+				max = Math.max(max, min * (j-i+1));
+			}
 		}
 		return max;
 	}
