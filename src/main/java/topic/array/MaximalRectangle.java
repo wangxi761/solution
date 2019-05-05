71e0bc1fea1f7bedea0a2de5b3f19ea7069fde99
wangxi761 <346461036@qq.com>
2019-05-05 16:02:11 +0800

    0505

--- src/main/java/topic/array/MaximalRectangle.java
+++ src/main/java/topic/array/MaximalRectangle.java
@@ -6,12 +6,20 @@ public class MaximalRectangle {
 	public int maximalRectangle(char[][] matrix) {
 		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
 		int[][] height = new int[matrix.length][matrix[0].length];
+		int max = 0;
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
 				height[i][j] = matrix[i][j] == '1' ? 1 + (i == 0 ? 0 : height[i - 1][j]) : 0;
 			}
+			int[] heights=height[i];
+			for (int index = 0; index < heights.length; index++) {
+				int min = heights[index];
+				for (int j = index; j < heights.length; j++) {
+					min = Math.min(heights[j], min);
+					max = Math.max(max, min * (j - index + 1));
+				}
+			}
 		}
-		System.out.println(Arrays.deepToString(height));
-		return 0;
+		return max;
 	}
 }
--- src/test/java/topic/array/MaximalRectangleTest.java
+++ src/test/java/topic/array/MaximalRectangleTest.java
@@ -9,12 +9,11 @@ public class MaximalRectangleTest {
 	@Test
 	public void maximalRectangle() {
 		MaximalRectangle maximalRectangle = new MaximalRectangle();
-		char[][] matrix = {
+		assertEquals(6,maximalRectangle.maximalRectangle(new char[][]{
 				{'1', '0', '1', '0', '0'},
 				{'1', '0', '1', '1', '1'},
 				{'1', '1', '1', '1', '1'},
 				{'1', '0', '0', '1', '0'}
-		};
-		maximalRectangle.maximalRectangle(matrix);
+		}));
 	}
 }
