51ed322f5a80b70944a7ea883312c98bc0526a05
wangxi761 <346461036@qq.com>
2019-05-06 01:19:43 +0800

    0506

---
+++ src/test/java/topic/array/ConstructBinaryTreefromPreorderandInorderTraversalTest.java
@@ -0,0 +1,16 @@
+package topic.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConstructBinaryTreefromPreorderandInorderTraversalTest {
+
+	@Test
+	public void buildTree() {
+		int[] preorder = {3, 9, 20, 15, 7};
+		int[] inorder = {9, 3, 15, 20, 7};
+		ConstructBinaryTreefromPreorderandInorderTraversal constructBinaryTreefromPreorderandInorderTraversal = new ConstructBinaryTreefromPreorderandInorderTraversal();
+		constructBinaryTreefromPreorderandInorderTraversal.buildTree(preorder, inorder);
+	}
+}
