0e84005a6f2169a7f228e410b660d1f767b95321
wangxi761 <346461036@qq.com>
2019-05-14 10:24:18 +0800

    0514

--- src/main/java/study/tree/binarySearchTree/DeleteNodeinaBST.java
+++ src/main/java/study/tree/binarySearchTree/DeleteNodeinaBST.java
@@ -4,31 +4,26 @@ import struct.TreeNode;
 
 public class DeleteNodeinaBST {
 	public TreeNode deleteNode(TreeNode root, int key) {
-		TreeNode cur=root,last=null;
-		while(cur!=null) {
-			if(cur.val==key) {
-				if(cur.left==null&&cur.right==null) {
-					last=null;
-				}else if(cur.left!=null&&cur.right!=null) {
-					TreeNode maxNode=cur.left;
-					int lMax=Integer.MIN_VALUE;
-					while(maxNode!=null) {
-						lMax=Math.max(lMax, maxNode.val);
-						maxNode=cur.right;
-					}
-					
-				}else {
-					TreeNode t=cur.left==null?cur.right:cur.left;
-					if(last.val>t.val) last.left=t;
-					else last.right=t;
-				}
-			}else if(cur.val>key) {
-				last=cur;
-				cur=cur.left;
-			}else if(cur.val<key) {
-				last=cur;
-				cur=cur.right;
-			}
+		if (root == null)
+			return null;
+		else if (root.val > key)
+			root.left = deleteNode(root.left, key);
+		else if (root.val < key)
+			root.right = deleteNode(root.right, key);
+		else {
+			if(root.left==null)
+				return root.right;
+			else if(root.right==null)
+				return root.left;
+			TreeNode min=findMin(root.right);
+			root.val=min.val;
+			root.right=deleteNode(root.right, root.val);
+		}
+		return root;
+	}
+	public TreeNode findMin(TreeNode root) {
+		while (root.left != null) {
+			root = root.left;
 		}
 		return root;
 	}
