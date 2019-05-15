0582c45688dbab5246fb10f1e021602462d863fd
wangxi761 <346461036@qq.com>
2019-05-15 11:20:29 +0800

    0515

---
+++ src/test/java/study/tree/binarySearchTree/KthLargestTest.java
@@ -0,0 +1,14 @@
+package study.tree.binarySearchTree;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KthLargestTest {
+	KthLargest test = new KthLargest(3, new int[]{4, 5, 8, 2});
+	@Test
+	public void testAdd() {
+		test.add(0);
+	}
+
+}
