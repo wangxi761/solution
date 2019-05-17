2568c7e0918f19211a1b9c2f3d4b38bcbfc0c39b
wangxi761 <346461036@qq.com>
2019-05-17 09:49:46 +0800

    0517

--- src/main/java/study/sort/QuickSort.java
+++ src/main/java/study/sort/QuickSort.java
@@ -18,13 +18,11 @@ public class QuickSort implements Sort {
 			while (arr[j] > pivot) {
 				j--;
 			}
-			if(i<j) {
+			if(i<=j) {
 				int t=arr[i];
 				arr[i]=arr[j];
 				arr[j]=t;
 				i++;j--;
-			}else if(i==j) {
-				i++;
 			}
 		}
 		sort(arr,head,j);
