d169c8122612b951c98f5a08e01c57b183dce03e
wangxi761 <346461036@qq.com>
2019-05-21 22:56:38 +0800

    0521

--- src/main/java/topic/string/ReorderLogFiles.java
+++ src/main/java/topic/string/ReorderLogFiles.java
@@ -1,27 +1,20 @@
 package topic.string;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class ReorderLogFiles {
+	
+	
 	public String[] reorderLogFiles(String[] logs) {
-		List<String> numbers = new ArrayList<>();
-		List<String> letter = new ArrayList<>();
-
-		for (String i : logs) {
-			String[] strs = i.split(" ");
-			if (Character.isDigit(strs[1].charAt(0))) {
-				numbers.add(i);
-			} else {
-				letter.add(i);
-			}
-		}
-		letter.sort(Comparator.comparing(i -> i.substring(i.indexOf(" ") + 1)));
-		letter.addAll(numbers);
-		Comparator<String> c=Comparator.comparing(i -> i.substring(i.indexOf(" ") + 1))	;
-		return letter.toArray(new String[letter.size()]);
+		Arrays.sort(logs, Comparator.comparing(s -> s.substring(s.indexOf(' ') + 1) + s.substring(0, s.indexOf(' ')), (o1, o2) -> {
+			if (Character.isLetter(o1.charAt(0)) && Character.isDigit(o2.charAt(0))) return -1;
+			else if (Character.isDigit(o1.charAt(0)) && Character.isLetter(o2.charAt(0))) return 1;
+			else if (Character.isDigit(o1.charAt(0)) && Character.isDigit(o2.charAt(0))) return 0;
+			else return o1.compareTo(o2);
+		}));
+		return logs;
 	}
 }
-//Comparator.comparing(i -> i.substring(i.indexOf(" ") + 1))
-//.thenComparing(i -> i.split(" ")[0])
