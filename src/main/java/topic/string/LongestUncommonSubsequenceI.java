5e2a8af56a31024c5c214a0db6e8286978556350
wangxi761 <346461036@qq.com>
2019-05-23 17:02:19 +0800

    0523

---
+++ src/main/java/topic/string/LongestUncommonSubsequenceI.java
@@ -0,0 +1,9 @@
+package topic.string;
+
+public class LongestUncommonSubsequenceI {
+	public int findLUSlength(String a, String b) {
+		if(a.equals(b))
+			return -1;
+		return Math.max(a.length(), b.length());
+	}
+}
