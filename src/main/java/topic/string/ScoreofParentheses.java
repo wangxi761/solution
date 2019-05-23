4acd0242601fb79a25ac1ef19dc94caca3f2ab1d
wangxi761 <346461036@qq.com>
2019-05-23 17:31:58 +0800

    0523

---
+++ src/main/java/topic/string/ScoreofParentheses.java
@@ -0,0 +1,17 @@
+package topic.string;
+
+import java.util.Stack;
+
+public class ScoreofParentheses {
+	public int scoreOfParentheses(String S) {
+		Stack<Character> stack=new Stack<>();
+		int i=0;
+		while(i<S.length()) {
+			char c = S.charAt(i);
+			if(stack.isEmpty()) {
+		
+			}
+		}
+		return 0;
+	}
+}
