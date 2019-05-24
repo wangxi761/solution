8ffd3970e4b48792b35584291603182a278a7705
wangxi761 <346461036@qq.com>
2019-05-24 10:35:19 +0800

    0524

--- src/main/java/topic/string/ScoreofParentheses.java
+++ src/main/java/topic/string/ScoreofParentheses.java
@@ -1,17 +1,18 @@
 package topic.string;
 
-import java.util.Stack;
-
 public class ScoreofParentheses {
 	public int scoreOfParentheses(String S) {
-		Stack<Character> stack=new Stack<>();
-		int i=0;
-		while(i<S.length()) {
-			char c = S.charAt(i);
-			if(stack.isEmpty()) {
-		
+		int result = 0, depth = 0;
+		for (int i = 0; i < S.length(); i++) {
+			if (S.charAt(i) == '(') {
+				depth++;
+			} else {
+				depth--;
+				if (S.charAt(i - 1) == '(') {
+					result += 1 << depth;
+				}
 			}
 		}
-		return 0;
+		return result;
 	}
 }
---
+++ src/test/java/topic/string/ScoreofParenthesesTest.java
@@ -0,0 +1,14 @@
+package topic.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScoreofParenthesesTest {
+
+	@Test
+	public void testScoreOfParentheses() {
+		System.out.println(1<<1);
+	}
+
+}
