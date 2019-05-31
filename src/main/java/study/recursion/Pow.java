5aba17213a8442ce963cb66829da9e12ed478df8
wangxi761 <346461036@qq.com>
2019-06-01 00:37:47 +0800

    0531

--- src/main/java/study/recursion/Pow.java
+++ src/main/java/study/recursion/Pow.java
@@ -1,5 +1,17 @@
 package study.recursion;
 
 public class Pow {
-
+	public double myPow(double x, int n) {
+		if (n == 0) {
+			return 1;
+		}
+		if (n < 0) {
+			if (n == Integer.MIN_VALUE) {
+				n += 2;
+			}
+			n = -n;
+			x = 1 / x;
+		}
+		return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+	}
 }
---
+++ src/test/java/study/recursion/PowTest.java
@@ -0,0 +1,14 @@
+package study.recursion;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PowTest {
+	Pow test = new Pow();
+	
+	@Test
+	public void myPow() {
+		System.out.println(Math.pow(-1, Integer.MIN_VALUE));
+	}
+}
