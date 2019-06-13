f6735faaaeb1d7b30cc5c92cc21e927482bf34d5
wangxi761 <346461036@qq.com>
2019-06-13 14:37:41 +0800

    0613

--- src/main/java/topic/Backtracking/LetterTilePossibilities.java
+++ src/main/java/topic/Backtracking/LetterTilePossibilities.java
@@ -5,25 +5,20 @@ public class LetterTilePossibilities {
 		if (tiles.length() == 0) return 0;
 		int[] dp = new int[26];
 		for (int i = 0; i < tiles.length(); i++) {
-			dp[tiles.charAt(i)]++;
+			dp[tiles.charAt(i) - 'A']++;
 		}
-		int sum = 0;
-		for (int i = 0; i < tiles.length(); i++) {
-			sum += recursive(dp, i);
-		}
-		return sum;
+		return recursive(dp);
 	}
 	
-	public int recursive(int[] dp, int n) {
-		if (n == 0) {
-			int sum = 0;
-			for (int i = 0; i < dp.length; i++) {
-				if (dp[i] > 0) {
-					sum++;
-				}
-			}
-			return sum;
+	public int recursive(int[] dp) {
+		int sum = 0;
+		for (int i = 0; i < dp.length; i++) {
+			if (dp[i] == 0) continue;
+			sum++;
+			dp[i]--;
+			sum += recursive(dp);
+			dp[i]++;
 		}
-		return 0;
+		return sum;
 	}
 }
