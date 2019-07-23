2c7ea4606994e09f07c4d459532bf0a2b83e0e52
wangxi761 <346461036@qq.com>
2019-07-23 09:54:27 +0800

    0723

--- src/main/java/solution/_31_40/ValidSudoku.java
+++ src/main/java/solution/_31_40/ValidSudoku.java
@@ -5,15 +5,40 @@ import java.util.Set;
 
 public class ValidSudoku {
 	public boolean isValidSudoku(char[][] board) {
-		Set<Character>[] row = new HashSet[9];
-		Set<Character>[] column = new HashSet[9];
-		Set<Character>[] square = new HashSet[9];
+		Set<Character>[] row = new Set[9];
+		Set<Character>[] column = new Set[9];
+		Set<Character>[] square = new Set[9];
 		
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				row[i].add(board[i][j]);
-				column[j].add(board[i][j]);
+				if (board[i][j] == '.') continue;
+				if (row[i] == null) {
+					row[i] = new HashSet<>();
+				}
+				if (!row[i].add(board[i][j])) {
+					return false;
+				}
+				
+				if (column[j] == null) {
+					column[j] = new HashSet<>();
+				}
+				if (!column[j].add(board[i][j])) {
+					return false;
+				}
+				
+				int index = index(i, j);
+				if (square[index] == null) {
+					square[index] = new HashSet<>();
+				}
+				if (!square[index].add(board[i][j])) {
+					return false;
+				}
 			}
 		}
+		return true;
+	}
+	
+	public int index(int i, int j) {
+		return i / 3 * 3 + j / 3;
 	}
 }
