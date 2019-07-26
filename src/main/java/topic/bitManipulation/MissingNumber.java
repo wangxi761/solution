513274a7ff54ed047dbdad10664542abf9e3bae6
wangxi761 <346461036@qq.com>
2019-07-26 16:32:07 +0800

    0726

--- src/main/java/topic/bitManipulation/MissingNumber.java
+++ src/main/java/topic/bitManipulation/MissingNumber.java
@@ -1,11 +1,20 @@
 package topic.bitManipulation;
 
 public class MissingNumber {
-	public int missingNumber(int[] nums) {
-		int i = 0;
+	public int missingNumber1(int[] nums) {
+		int sum = 0;
 		for (int num : nums) {
-			i |= (1 << (num - 1));
+			sum += num;
 		}
-		return i;
+		return (nums.length + 1) * nums.length / 2 - sum;
+	}
+	
+	public int missingNumber(int[] nums) {
+		int res = nums.length;
+		for (int i = 0; i < nums.length; i++) {
+			res ^= i;
+			res ^= nums[i];
+		}
+		return res;
 	}
 }
--- src/test/java/topic/bitManipulation/MissingNumberTest.java
+++ src/test/java/topic/bitManipulation/MissingNumberTest.java
@@ -2,7 +2,7 @@ package topic.bitManipulation;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class MissingNumberTest {
 	MissingNumber test = new MissingNumber();
@@ -11,5 +11,7 @@ public class MissingNumberTest {
 	public void missingNumber() {
 		assertEquals(2, test.missingNumber(new int[]{3, 0, 1}));
 		assertEquals(8, test.missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+		assertEquals(33, test.missingNumber(new int[]{44, 26, 34, 25, 23, 42, 0, 43, 38, 14, 47, 19, 49, 6, 16, 41, 24, 35, 10, 4, 32, 5, 8, 15, 31, 3, 46, 22, 2, 30, 28, 37, 1, 21, 39, 45, 9, 48, 36, 17, 7, 27, 18, 29, 13, 40, 11, 20, 12}));
+		
 	}
 }
