687666fbe92e5a3699ca3d22c8b8e77d6998ccdf
wangxi761 <346461036@qq.com>
2019-08-01 15:06:04 +0800

    0801

--- src/main/java/topic/trie/LongestWordinDictionary.java
+++ src/main/java/topic/trie/LongestWordinDictionary.java
@@ -19,25 +19,46 @@ public class LongestWordinDictionary {
 					node.nodes = new Node[26];
 				}
 				int ch = word.charAt(i) - 97;
-				node.nodes[ch] = new Node(i == word.toCharArray().length - 1);
+				if (node.nodes[ch] == null) {
+					node.nodes[ch] = new Node(i == word.toCharArray().length - 1);
+				} else {
+					node.nodes[ch].used |= i == word.toCharArray().length - 1;
+				}
 				node = node.nodes[ch];
 			}
 		}
 		StringBuilder sb = new StringBuilder();
-		StringBuilder res = new StringBuilder();
-		recursive(head, sb, res);
-		return res.toString();
+//		StringBuilder res = new StringBuilder();
+//		recursive(head, sb, res);
+//		return res.toString();
+		return recursive(head, sb);
 	}
 	
-	public void recursive(Node head, StringBuilder sb, StringBuilder res) {
-		if (head == null || !head.used) return;
-		if (head.nodes == null) {
-			if (res.length() < sb.length()) {
-				res.replace(0, res.length(), sb.toString());
-			}
-			return;
+	public String recursive(Node head, StringBuilder sb) {
+		if (!head.used) {
+			return sb.substring(0, sb.length() - 1).toString();
 		}
+		if (head.nodes == null) {
+			return sb.toString();
+		}
+		String res = "";
 		for (int i = 0; i < head.nodes.length; i++) {
+			if (head.nodes[i] == null) continue;
+			sb.append((char) (i + 97));
+			String recursive = recursive(head.nodes[i], sb);
+			if (recursive.length() > res.length()) res = recursive;
+			sb.delete(sb.length() - 1, sb.length());
+		}
+		return res;
+	}
+	public void recursive(Node head, StringBuilder sb, StringBuilder res) {
+		if (!head.used) return;
+		if (res.length() < sb.length()) {
+			res.replace(0, res.length(), sb.toString());
+		}
+		if (head.nodes == null) return;
+		for (int i = 0; i < head.nodes.length; i++) {
+			if (head.nodes[i] == null) continue;
 			sb.append((char) (i + 97));
 			recursive(head.nodes[i], sb, res);
 			sb.delete(sb.length() - 1, sb.length());
