24ba20394e047d8555669ed9573e869bfb8993f3
wangxi761 <346461036@qq.com>
2019-08-02 11:42:03 +0800

    0802

---
+++ src/main/java/topic/sort/MatrixCellsinDistanceOrder.java
@@ -0,0 +1,27 @@
+package topic.sort;
+
+public class MatrixCellsinDistanceOrder {
+	public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+		int[][] res = new int[R * C][2];
+		int count = 0;
+		for (int i = 0; i <= R + C - 2; i++) {
+			for (int x = 0; x <= i; x++) {
+				int y = i - x;
+				count = getCount(R, C, res, count, r0 + x, c0 + y);
+				count = getCount(R, C, res, count, r0 - x, c0 + y);
+				count = getCount(R, C, res, count, r0 + x, c0 - y);
+				count = getCount(R, C, res, count, r0 - x, c0 - y);
+			}
+		}
+		return res;
+	}
+	
+	private int getCount(int R, int C, int[][] res, int count, int x1, int y1) {
+		if (x1 >= 0 && x1 < R && y1 >= 0 && y1 < C) {
+			res[count][0] = x1;
+			res[count][1] = y1;
+			count++;
+		}
+		return count;
+	}
+}
