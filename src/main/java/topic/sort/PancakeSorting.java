f06e815c1756a66de08ad9eb3b1edda3ac519be5
wangxi761 <346461036@qq.com>
2019-08-06 15:10:23 +0800

    0806

--- src/main/java/topic/sort/PancakeSorting.java
+++ src/main/java/topic/sort/PancakeSorting.java
@@ -6,15 +6,41 @@ import java.util.List;
 public class PancakeSorting {
 	public List<Integer> pancakeSort(int[] A) {
 		List<Integer> res = new ArrayList<>();
-		
+		for (int i = A.length - 1; i >= 0; i--) {
+			int id = findMax(A, 0, i + 1);
+			if (id == i) {
+				continue;
+			}
+			reverse(A, 0, id + 1, res);
+			reverse(A, 0, i + 1, res);
+		}
+		return res;
 	}
 	
 	public int findMax(int[] a, int begin, int end) {
-		int max = Integer.MIN_VALUE;
+		if (begin == end) return begin;
+		int max = Integer.MIN_VALUE, index = -1;
 		for (int i = begin; i < end; i++) {
-			max = Math.max(max, a[i]);
+			if (a[i] > max) {
+				max = a[i];
+				index = i;
+			}
 		}
-		return max;
+		return index;
 	}
 	
+	public void reverse(int[] arr, int begin, int end, List<Integer> res) {
+		end--;
+		if (begin >= end) {
+			return;
+		}
+		res.add(end + 1);
+		while (begin < end) {
+			int tmp = arr[begin];
+			arr[begin] = arr[end];
+			arr[end] = tmp;
+			begin++;
+			end--;
+		}
+	}
 }
---
+++ src/test/java/topic/sort/PancakeSortingTest.java
@@ -0,0 +1,13 @@
+package topic.sort;
+
+import org.junit.Test;
+
+public class PancakeSortingTest {
+	PancakeSorting test = new PancakeSorting();
+	
+	@Test
+	public void pancakeSort() {
+		System.out.println(test.pancakeSort(new int[]{3, 2, 4, 1}));
+		System.out.println(test.pancakeSort(new int[]{1, 2, 3}));
+	}
+}
