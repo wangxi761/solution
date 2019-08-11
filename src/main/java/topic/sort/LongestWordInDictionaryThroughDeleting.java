4082b385c1d6ec918208d6c5388db8c902548120
wangxi761 <346461036@qq.com>
2019-08-12 02:30:52 +0800

    0812

---
+++ src/main/java/topic/sort/LongestWordInDictionaryThroughDeleting.java
@@ -0,0 +1,31 @@
+package topic.sort;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LongestWordInDictionaryThroughDeleting {
+	public String findLongestWord(String s, List<String> d) {
+		int[][] candiate = new int[d.size()][26];
+		int[] res = new int[d.size()];
+		for (char c : s.toCharArray()) {
+			for (int i = 0; i < d.size(); i++) {
+				candiate[i][c - 'a']++;
+			}
+		}
+		for (int i = 0; i < d.size(); i++) {
+			for (char c : d.get(i).toCharArray()) {
+				candiate[i][c - 'a']--;
+			}
+			for (int j = 0; j < candiate[i].length; j++) {
+				if (candiate[i][j] < 0) {
+					res[i] = -1;
+					break;
+				}
+				if (candiate[i][j] > 0) {
+					res[i] += 1;
+				}
+			}
+		}
+		
+	}
+}
