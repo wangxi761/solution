f37695e1484662cf44f768066930cf8fad682f47
wangxi761 <346461036@qq.com>
2019-08-19 23:44:07 +0800

    0819

---
+++ src/test/java/topic/hashtable/DesignHashMapTest.java
@@ -0,0 +1,12 @@
+package topic.hashtable;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DesignHashMapTest {
+	@Test
+	public void test() {
+		System.out.println(Math.pow(2, 20));
+	}
+}
