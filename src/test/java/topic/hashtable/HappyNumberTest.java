b9e15cb49c8ce756b34490f856e692004560e619
wangxi761 <346461036@qq.com>
2019-08-23 10:29:10 +0800

    0823

---
+++ src/test/java/topic/hashtable/HappyNumberTest.java
@@ -0,0 +1,12 @@
+package topic.hashtable;
+
+import org.junit.Test;
+
+public class HappyNumberTest {
+	HappyNumber test = new HappyNumber();
+	
+	@Test
+	public void isHappy() {
+		System.out.println(test.isHappy(2));
+	}
+}
