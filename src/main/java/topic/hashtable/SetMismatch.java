b18f1a788e6b1b740f80322f6f4da8bb1ff8caa5
wangxi761 <346461036@qq.com>
2019-08-23 16:59:35 +0800

    0823

--- src/main/java/topic/hashtable/SetMismatch.java
+++ src/main/java/topic/hashtable/SetMismatch.java
@@ -25,4 +25,18 @@ public class SetMismatch {
 		nums[i] = nums[j];
 		nums[j] = data;
 	}
+	
+	public int findDuplicate(int[] nums) {
+		int slow = 0, fast = 0;
+		do {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		} while (slow != fast);
+		fast = 0;
+		while (fast != slow) {
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return fast;
+	}
 }
--- src/test/java/topic/hashtable/SetMismatchTest.java
+++ src/test/java/topic/hashtable/SetMismatchTest.java
@@ -1,13 +1,12 @@
 package topic.hashtable;
 
 import org.junit.Test;
-import util.ArrayUtil;
 
 public class SetMismatchTest {
 	SetMismatch test = new SetMismatch();
 	
 	@Test
 	public void findErrorNums() {
-		ArrayUtil.prettyPrint(test.findErrorNums(new int[]{3, 2, 2}));
+		System.out.println(test.findDuplicate(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 6}));
 	}
 }
