b58edf40523a97eeaeea06a1539d11eb8b8e280e
wangxi761 <346461036@qq.com>
2019-08-23 11:43:37 +0800

    0823

---
+++ src/main/java/topic/hashtable/SetMismatch.java
@@ -0,0 +1,15 @@
+package topic.hashtable;
+
+public class SetMismatch {
+	public int[] findErrorNums(int[] nums) {
+		int[] res = new int[2];
+		int i = 0;
+		while (true) {
+			if (i == nums[i]) continue;
+			i = nums[i];
+			if (i == nums[i]) {
+			
+			}
+		}
+	}
+}
