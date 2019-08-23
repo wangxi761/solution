310b303f033d373153fa3e03b241fb23f89ed633
wangxi761 <346461036@qq.com>
2019-08-23 11:17:07 +0800

    0823

---
+++ src/test/java/topic/hashtable/LongestHarmoniousSubsequenceTest.java
@@ -0,0 +1,12 @@
+package topic.hashtable;
+
+import org.junit.Test;
+
+public class LongestHarmoniousSubsequenceTest {
+	LongestHarmoniousSubsequence test = new LongestHarmoniousSubsequence();
+	
+	@Test
+	public void findLHS() {
+		System.out.println(test.findLHS(new int[]{1, 3, 2, 2, 5, 2, 3, 7}));
+	}
+}
