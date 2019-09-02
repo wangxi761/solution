aab5c053cd4b3bec2e2776debc93191793ae8beb
wangxi761 <346461036@qq.com>
2019-09-02 11:55:03 +0800

    0902

--- src/main/java/topic/heap/TopKFrequentElements.java
+++ src/main/java/topic/heap/TopKFrequentElements.java
@@ -16,4 +16,25 @@ public class TopKFrequentElements {
 		}
 		return res;
 	}
+	
+	public List<Integer> topKFrequent1(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int num : nums) {
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+		PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Comparator.comparing(Map.Entry::getValue));
+		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+			if (queue.size() < k) {
+				queue.offer(entry);
+			} else if (queue.peek().getValue() < entry.getValue()) {
+				queue.poll();
+				queue.offer(entry);
+			}
+		}
+		List<Integer> res = new ArrayList<>();
+		while (!queue.isEmpty()) {
+			res.add(0, queue.poll().getKey());
+		}
+		return res;
+	}
 }
