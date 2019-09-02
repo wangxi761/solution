2ff763bf6d59638ac860e186875c3e04ccd90aee
wangxi761 <346461036@qq.com>
2019-09-02 16:22:25 +0800

    0902

--- src/main/java/topic/heap/TopKFrequentElements.java
+++ src/main/java/topic/heap/TopKFrequentElements.java
@@ -18,12 +18,12 @@ public class TopKFrequentElements {
 	}
 	
 	public List<Integer> topKFrequent1(int[] nums, int k) {
-		Map<Integer, Integer> map = new HashMap<>();
+		Map<Integer, Integer> cache = new HashMap<>();
 		for (int num : nums) {
-			map.put(num, map.getOrDefault(num, 0) + 1);
+			cache.put(num, cache.getOrDefault(num, 0) + 1);
 		}
 		PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Comparator.comparing(Map.Entry::getValue));
-		for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+		for (Map.Entry<Integer, Integer> entry : cache.entrySet()) {
 			if (queue.size() < k) {
 				queue.offer(entry);
 			} else if (queue.peek().getValue() < entry.getValue()) {
@@ -34,6 +34,27 @@ public class TopKFrequentElements {
 		List<Integer> res = new ArrayList<>();
 		while (!queue.isEmpty()) {
 			res.add(0, queue.poll().getKey());
+		}
+		return res;
+	}
+	
+	public List<Integer> topKFrequent2(int[] nums, int k) {
+		Map<Integer, Integer> cache = new HashMap<>();
+		for (int num : nums) {
+			cache.put(num, cache.getOrDefault(num, 0) + 1);
+		}
+		List<Integer>[] buckets = new List[nums.length + 1];
+		for (Map.Entry<Integer, Integer> entry : cache.entrySet()) {
+			if (buckets[entry.getValue()] == null) {
+				buckets[entry.getValue()] = new ArrayList<>();
+			}
+			buckets[entry.getValue()].add(entry.getKey());
+		}
+		List<Integer> res = new ArrayList<>();
+		for (int i = buckets.length - 1; i >= 0 && res.size() < k; i--) {
+			if (buckets[i] != null) {
+				res.addAll(buckets[i]);
+			}
 		}
 		return res;
 	}
---
+++ src/test/java/topic/heap/TopKFrequentElementsTest.java
@@ -0,0 +1,13 @@
+package topic.heap;
+
+import org.junit.Test;
+
+public class TopKFrequentElementsTest {
+	TopKFrequentElements test = new TopKFrequentElements();
+	
+	@Test
+	public void topKFrequent2() {
+		System.out.println(test.topKFrequent2(new int[]{1}, 1));
+		System.out.println(test.topKFrequent2(new int[]{1, 1, 1, 2, 2, 3}, 2));
+	}
+}
