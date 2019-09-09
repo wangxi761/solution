d7ed377b62be45049c7953cf404275d78b5623cf
wangxi761 <346461036@qq.com>
2019-09-09 11:43:57 +0800

    0909

--- src/main/java/topic/heap/NetworkDelayTime.java
+++ src/main/java/topic/heap/NetworkDelayTime.java
@@ -5,22 +5,15 @@ import java.util.Map;
 
 public class NetworkDelayTime {
 	
+	
 	public int networkDelayTime(int[][] times, int N, int K) {
 		Map<Integer, Map<Integer, Integer>> cache = new HashMap<>();
 		for (int[] time : times) {
-			Map<Integer, Integer> map = cache.getOrDefault(time[0], new HashMap<>());
-			map.put(time[1], time[2]);
-			cache.put(time[0], map);
+			cache.putIfAbsent(time[0], new HashMap<>());
+			cache.get(time[0]).put(time[1], time[2]);
 		}
+		
 	}
 	
-	public void recursive(Map<Integer, Map<Integer, Integer>> cache, int k, int len) {
-		if (!cache.containsKey(k)) {
-			return;
-		}
-		for (Map.Entry<Integer, Integer> entry : cache.get(k).entrySet()) {
-			entry
-		}
-	}
 	
 }
