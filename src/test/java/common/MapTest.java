a2b207ceccb1b1ec9ae2a9c5ae108391294966c7
wangxi761 <346461036@qq.com>
2019-09-18 22:16:33 +0800

    0918

--- src/test/java/common/MapTest.java
+++ src/test/java/common/MapTest.java
@@ -2,15 +2,47 @@ package common;
 
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapTest {
 	@Test
 	public void test() {
-		HashSet<Integer> set = new HashSet<>();
-		for (int i = 0; i < 12; i++) {
-			set.add(i);
+		String str = "123a4b5c";
+		List<Integer> list = parseInt1(str);
+		System.out.println(list);
+	}
+	
+	private List<Integer> parseInt1(String str) {
+		List<Integer> list = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
+		for (char c : str.toCharArray()) {
+			if (!Character.isDigit(c) && sb.length() > 0) {
+				list.add(Integer.valueOf(sb.toString()));
+				sb.delete(0, sb.length());
+			} else if (Character.isDigit(c)) {
+				sb.append(c);
+			}
 		}
+		return list;
+	}
+	
+	private List<Integer> parseInt(String str) {
+		List<Integer> list = new ArrayList<>();
+		int l = 0, r = 0;
+		while (l < str.length() && r < str.length()) {
+			while (l < str.length() && !Character.isDigit(str.charAt(l))) {
+				l++;
+			}
+			r = l;
+			while (l < str.length() && Character.isDigit(str.charAt(r))) {
+				r++;
+			}
+			if (l < r) {
+				list.add(Integer.valueOf(str.substring(l, r)));
+			}
+			l = r;
+		}
+		return list;
 	}
 }
