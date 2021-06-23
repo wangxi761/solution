8827e57e94295d2da3630934e20552a5858ea55a
wangxi <wangxi3@huya.com>
2021-06-24 01:17:04 +0800

    bb

--- src/main/java/bit/NumberOfMatchingSubsequences.java
+++ src/main/java/bit/NumberOfMatchingSubsequences.java
@@ -3,40 +3,38 @@ package bit;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberOfMatchingSubsequences {
 	
 	public int numMatchingSubseq(String s, String[] words) {
-		Set<String> cache = new HashSet<>();
-		int sum = 0;
-		for (String word : words) {
-			if (isSub(s, word, cache)) sum++;
+		Map<Character, Deque<String>> cache = new HashMap<>();
+		for (char i = 'a'; i <= 'z'; i++) {
+			cache.put(i, new LinkedList<>());
 		}
-		return sum;
+		for (String word : words) {
+			cache.get(word.charAt(0)).addLast(word);
+		}
+		
+		int count=0;
+		for (char c : s.toCharArray()) {
+			Deque<String> deque = cache.get(c);
+			int size = deque.size();
+			for (int i = 0; i < size; i++) {
+				String s1 = deque.removeFirst();
+				if (s1.length() == 1) {
+					count++;
+				} else {
+					cache.get(s1.charAt(1)).addLast(s1.substring(1));
+				}
+			}
+		}
+		return count;
 	}
 	
-	public boolean isSub(String s, String word, Set<String> cache) {
-		if (cache.contains(word)) {
-			return true;
-		}
-		int cur = 0;
-		for (int i = 0; i < word.length(); i++) {
-			String str = word.substring(i, i + 1);
-			int index = s.indexOf(str, cur);
-			if (index < 0) {
-				return false;
-			}
-			cache.add(str);
-			cur = index + 1;
-		}
-		return true;
-	}
+
 	
 	
 	@Test
