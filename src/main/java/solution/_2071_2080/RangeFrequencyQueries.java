3d6bfd4c8656e5de25f7f59613e7117135ddbcaf
wangxi <346461036@qq.com>
2023-06-19 01:21:55 +0800

    binary search

--- src/main/java/solution/_2071_2080/RangeFrequencyQueries.java
+++ src/main/java/solution/_2071_2080/RangeFrequencyQueries.java
@@ -1,7 +1,8 @@
 package solution._2071_2080;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RangeFrequencyQueries {
 	
@@ -23,6 +24,46 @@ public class RangeFrequencyQueries {
 			}
 			cache.put(key, count);
 			return count;
+		}
+	}
+	
+	public static class RangeFreqQueryI {
+		private final Map<Integer, List<Integer>> map = new HashMap<>();
+		
+		public RangeFreqQueryI(int[] arr) {
+			for (int i = 0; i < arr.length; i++) {
+				List<Integer> list = map.getOrDefault(arr[i], new ArrayList<>());
+				list.add(i);
+				map.put(arr[i], list);
+			}
+		}
+		
+		public int query(int left, int right, int value) {
+			List<Integer> its = map.get(value);
+			if (its == null) return 0;
+			int i = Collections.binarySearch(its, left);
+			int j = Collections.binarySearch(its, right);
+			if (i < 0) i = -i - 1;
+			if (j < 0) j = -j - 2;
+			return j - i + 1;
+		}
+	}
+	
+	
+	static class CodeTest {
+		public static void main(String[] args) {
+			int[] arr = {12, 33, 4, 56, 22, 2, 34, 33, 22, 12, 34, 56};
+			TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+			for (int i = 0; i < arr.length; i++) {
+				List<Integer> list = map.getOrDefault(arr[i], new ArrayList<>());
+				list.add(i);
+				map.put(arr[i], list);
+			}
+			map.forEach((k, v) -> System.out.println(k + " -> " + v));
+			List<Integer> its = map.get(4);
+			int i = Collections.binarySearch(its, 1);
+			int j = Collections.binarySearch(its, 5);
+			System.out.println(i + " " + j);
 		}
 	}
 }
