f1629d7de42e38f524e4cf2173392fbe88382c7b
wangxi <346461036@qq.com>
2023-06-20 23:57:04 +0800

    new stage

---
+++ src/main/java/solution/_1_100/Palindrome_Number/Solution.java
@@ -0,0 +1,18 @@
+package solution._1_100.Palindrome_Number;
+
+class Solution {
+	public boolean isPalindrome(int x) {
+		if (x < 0) {
+			return false;
+		}
+		if (x > 0 && x % 10 == 0) {
+			return false;
+		}
+		int rev = 0;
+		while (x > rev) {
+			rev = rev * 10 + x % 10;
+			x = x / 10;
+		}
+		return x == rev || x == rev / 10;
+	}
+}
