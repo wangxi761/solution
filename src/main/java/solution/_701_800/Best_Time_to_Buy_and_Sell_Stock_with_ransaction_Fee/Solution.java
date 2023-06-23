4b07b583c4748785d5eaf8085fd8feb4033496db
wangxi761 <346461036@qq.com>
2023-06-24 01:07:31 +0800

    0623

--- src/main/java/solution/_701_800/Best_Time_to_Buy_and_Sell_Stock_with_ransaction_Fee/Solution.java
+++ src/main/java/solution/_701_800/Best_Time_to_Buy_and_Sell_Stock_with_ransaction_Fee/Solution.java
@@ -3,8 +3,16 @@ package solution._701_800.Best_Time_to_Buy_and_Sell_Stock_with_ransaction_Fee;
 class Solution {
 	public int maxProfit(int[] prices, int fee) {
 		int n = prices.length;
-		int[] free = new int[n];
-		int[] hold = new int[n];
+		int[] free = new int[n], hold = new int[n];
 		
+		// In order to hold a stock on day 0, we have no other choice but to buy it for prices[0].
+		hold[0] = -prices[0];
+		
+		for (int i = 1; i < n; i++) {
+			hold[i] = Math.max(hold[i - 1], free[i - 1] - prices[i]);
+			free[i] = Math.max(free[i - 1], hold[i - 1] + prices[i] - fee);
+		}
+		
+		return free[n - 1];
 	}
 }
