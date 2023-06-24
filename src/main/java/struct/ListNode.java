3bd78127c3dd7641f6cca221d4a5e79e8ae84b2e
wangxi <346461036@qq.com>
2023-06-25 01:02:17 +0800

    0625

--- src/main/java/struct/ListNode.java
+++ src/main/java/struct/ListNode.java
@@ -3,43 +3,54 @@ package struct;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListNode {
 	public int val;
 	public ListNode next;
+	
 	public ListNode(int x) {
 		val = x;
 	}
 	
-	public static ListNode create(int...src) {
-		int[] arr=src;
-		if(arr.length==0)
-			return null;
-		ListNode head=new ListNode(0),node=head;
-		for (int i : arr) {
-			ListNode next=new ListNode(i);
-			node.next=next;
-			node=next;
-		}
-		return head.next;
-	}
-	
 	public String getList() {
-		List<Integer> list=new ArrayList<>();
-		ListNode head=this;
-		while(head!=null) {
+		List<Integer> list = new ArrayList<>();
+		ListNode head = this;
+		while (head != null) {
 			list.add(head.val);
-			head=head.next;
+			head = head.next;
 		}
 		return list.toString();
 	}
-
+	
 	@Override
 	public String toString() {
-		return "ListNode [val=" + val + "]";
+		return val + (next == null ? "" : " -> " + next);
 	}
 	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ListNode listNode = (ListNode) o;
+		return val == listNode.val && Objects.equals(next, listNode.next);
+	}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(val, next);
+	}
 	
+	public static ListNode create(int... src) {
+		if (src.length == 0)
+			return null;
+		ListNode head = new ListNode(0), node = head;
+		for (int i : src) {
+			ListNode next = new ListNode(i);
+			node.next = next;
+			node = next;
+		}
+		return head.next;
+	}
 	
 }
