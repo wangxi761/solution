a0b060e2be507cdd6d2568d364f9eb6ffc6e7159
wangxi <346461036@qq.com>
2023-07-13 00:15:00 +0800

    romanToInt

--- src/main/java/solution/_1_100/Roman_to_Integer/Solution.java
+++ src/main/java/solution/_1_100/Roman_to_Integer/Solution.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-	public int romanToInt(String s) {
+	public int romanToIntI(String s) {
 		Map<Character, Integer> romanCache = new HashMap<>();
 		romanCache.put('I', 1);
 		romanCache.put('V', 5);
@@ -36,11 +36,57 @@ class Solution {
 		return sum;
 	}
 	
+	public int romanToIntII(String s) {
+		Map<Character, Integer> romanCache = new HashMap<>();
+		romanCache.put('I', 1);
+		romanCache.put('V', 5);
+		romanCache.put('X', 10);
+		romanCache.put('L', 50);
+		romanCache.put('C', 100);
+		romanCache.put('D', 500);
+		romanCache.put('M', 1000);
+		
+		int sum = 0, last = 0;
+		if (s.length() == 1) {
+			return romanCache.get(s.charAt(0));
+		}
+		for (int i = 0; i < s.length() - 1; i++) {
+			Integer cur = romanCache.get(s.charAt(i));
+			Integer next = romanCache.get(s.charAt(i + 1));
+			sum = cur < next ? sum - cur : sum + cur;
+		}
+		sum += romanCache.get(s.charAt(s.length() - 1));
+		return sum;
+	}
+	
+	public int romanToInt(String s) {
+		Map<Character, Integer> romanCache = new HashMap<>();
+		romanCache.put('I', 1);
+		romanCache.put('V', 5);
+		romanCache.put('X', 10);
+		romanCache.put('L', 50);
+		romanCache.put('C', 100);
+		romanCache.put('D', 500);
+		romanCache.put('M', 1000);
+		
+		int sum = 0, last = 0;
+		for (int i = s.length() - 1; i >= 0; i--) {
+			int cur = romanCache.get(s.charAt(i));
+			if (cur < last) {
+				sum -= cur;
+			} else {
+				sum += cur;
+			}
+			last = cur;
+		}
+		return sum;
+	}
+	
 	@Test
 	public void test() {
+		Assertions.assertEquals(1994, romanToInt("MCMXCIV"));
 		Assertions.assertEquals(3, romanToInt("III"));
 		Assertions.assertEquals(58, romanToInt("LVIII"));
-		Assertions.assertEquals(1994, romanToInt("MCMXCIV"));
 		
 	}
 }
