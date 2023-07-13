bf9599646f9675a1b25c4fecbed57c99eaab5c0c
wangxi <346461036@qq.com>
2023-07-14 01:12:21 +0800

    longestCommonPrefix

--- src/main/java/solution/_1_100/Longest_Common_Prefix/Solution.java
+++ src/main/java/solution/_1_100/Longest_Common_Prefix/Solution.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class Solution {
-	public String longestCommonPrefix(String[] strs) {
+	public String longestCommonPrefixI(String[] strs) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 200; i++) {
 			char first = 0;
@@ -23,6 +23,30 @@ class Solution {
 				}
 			}
 			sb.append(first);
+		}
+		return sb.toString();
+	}
+	
+	public String longestCommonPrefix(String[] strs) {
+		StringBuilder sb = new StringBuilder();
+		int index = 0;
+		OUTER:
+		while (true) {
+			Character c = null;
+			for (String str : strs) {
+				if (index >= str.length()) {
+					break OUTER;
+				}
+				if (c == null) {
+					c = str.charAt(index);
+				} else {
+					if (c != str.charAt(index)) {
+						break OUTER;
+					}
+				}
+			}
+			sb.append(c);
+			index++;
 		}
 		return sb.toString();
 	}
