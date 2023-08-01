c48b09a709a6f7c99318324597d313e694c24744
wangxi <346461036@qq.com>
2023-08-01 23:36:44 +0800

    combine

--- src/main/java/leetcode/problems/_001_100/Combinations/Solution.java
+++ src/main/java/leetcode/problems/_001_100/Combinations/Solution.java
@@ -10,18 +10,18 @@ import java.util.Set;
 class Solution {
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> res = new ArrayList<>();
-		backtrack(res, new ArrayList<>(), 0, n, k);
+		backtrack(res, new ArrayList<>(), 1, n, k);
 		return res;
 	}
 	
 	public void backtrack(List<List<Integer>> res, List<Integer> cur, int start, int n, int k) {
 		if (k == 0) {
-			res.add(cur);
+			res.add(new ArrayList<>(cur));
 			return;
 		}
 		for (int i = start; i <= n; i++) {
 			cur.add(i);
-			backtrack(res, cur, start + 1, n, k - 1);
+			backtrack(res, cur, i + 1, n, k - 1);
 			cur.remove(cur.size() - 1);
 		}
 	}
