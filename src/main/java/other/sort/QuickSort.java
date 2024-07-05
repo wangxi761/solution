d15e07ad01a7cd3910ade7ada82cdc20a9c98c29
wangxi <346461036@qq.com>
2024-07-06 02:45:12 +0800

    qc2

--- src/main/java/other/sort/QuickSort.java
+++ src/main/java/other/sort/QuickSort.java
@@ -13,6 +13,27 @@ public class QuickSort {
 		System.out.println(Arrays.toString(arr));
 	}
 	
+	
+	public void quicksortII(int[] arr, int l, int r) {
+		if (l >= r) {
+			return;
+		}
+		int p = arr[l], lt = l, gt = r, i = l + 1;
+		
+		while (i <= gt) {
+			if (arr[i] < p) {
+				swap(arr, lt++, i++);
+			} else if (arr[i] > p) {
+				swap(arr, i, gt--);
+			} else {
+				i++;
+			}
+		}
+		quicksort(arr, l, lt - 1);
+		quicksort(arr, gt + 1, r);
+	}
+	
+	
 	public void quicksort(int[] arr, int l, int r) {
 		if (l < r) {
 			int p = partition(arr, l, r);
@@ -21,8 +42,24 @@ public class QuickSort {
 		}
 	}
 	
-	
 	public int partition(int[] arr, int l, int r) {
+		int p = arr[(l + r) / 2], i = l - 1, j = r + 1;
+		while (true) {
+			do {
+				i++;
+			} while (arr[i] < p);
+			do {
+				j--;
+			} while (arr[j] > p);
+			if (i >= j) {
+				return j;
+			}
+			swap(arr, i, j);
+		}
+	}
+	
+	
+	public int partitionI(int[] arr, int l, int r) {
 		int pv = arr[l + (r - l) / 2];
 		int i = l - 1;
 		int j = r + 1;
